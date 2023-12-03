811c461400dafed2534ea229a7a2fee13195fc7c
amitmodi06 <amitmodi06@gmail.com>
2023-12-03 18:16:14 +0000

    Basic to list App structure, showing all the to do items on the web page

--- mytodo/models.py
+++ mytodo/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Todo_Task(models.Model):
+    """Model representing a task."""
+    task = models.CharField(max_length=500, help_text="Enter a to do task")
+
+    class Meta:
+        pass
+
+    def __str__(self):
+        return self.task
+
+    def get_absolute_url(self):
+        """Returns the url to access a particular task instance."""
+        return reverse("task-detail", args-[str(self.id)])
--- mytodo/views.py
+++ mytodo/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from .models import Todo_Task
 
 # Create your views here.
+def sayHello(request):
+    task_list = Todo_Task.objects.all()
+    context = {"task_list": task_list}
+    return render(request, "todo.html", context)
